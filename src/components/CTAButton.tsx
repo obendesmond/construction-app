240bf20bba6eb2bf94a21f563f56dad451b79b10
Oben Desmond <39285324+obendesmond@users.noreply.github.com>
2024-04-27 06:07:17 +0100

    Update CTAButton padding left and right

--- src/components/CTAButton.tsx
+++ src/components/CTAButton.tsx
@@ -4,7 +4,7 @@ import Button from "./Button";
 const CTAButton = () => {
   return (
     <div className="flex flex-col md:flex-row justify-center lg:justify-start gap-6">
-      <Button text="Contact Us" className="px-14" />
+      <Button text="Contact Us" className="pl-14 pr-14" />
       <Button text="Free Consultation" outline />
     </div>
   );
